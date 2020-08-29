5a49950edb51df55f8de0b4a881ad4f79e44159f
Berkeley True <berkeleytrue@users.noreply.github.com>
2020-08-29 12:34:33 -0700

    feat(tf/pve/omv): plan complete for init vm

--- tf/main.tf
+++ tf/main.tf
@@ -5,19 +5,21 @@ provider "proxmox" {
   pm_tls_insecure = true
 }
 
+# proxmox_vm_qemu.omv:
 resource "proxmox_vm_qemu" "omv" {
   agent       = 1
   balloon     = 0
   bios        = "seabios"
-  boot        = "cdn"
+  boot        = "c"
   bootdisk    = "scsi0"
-  iso         = "local:iso/openmediavault_5.3.9-amd64.iso"
+  clone       = "openmediavault"
+  full_clone  = true
   cores       = 2
   cpu         = "host"
   hotplug     = "network,disk,usb"
   kvm         = true
   memory      = 2048
-  name        = "openmediavault"
+  name        = "omv"
   numa        = false
   onboot      = true
   qemu_os     = "l26"
@@ -25,7 +27,10 @@ resource "proxmox_vm_qemu" "omv" {
   sockets     = 2
   target_node = "pve"
   vcpus       = 0
-  vmid        = 101
+
+  ipconfig0 = "ip=dhcp,ip6=dhcp"
+
+  ciuser = "berkeleytrue"
 
   disk {
     id           = 0
@@ -36,8 +41,16 @@ resource "proxmox_vm_qemu" "omv" {
   }
 
   network {
-    id     = 0
-    model  = "virtio"
-    bridge = "vmbr0"
+    id      = 0
+    model   = "virtio"
+    bridge  = "vmbr0"
+    macaddr = "76:60:11:E4:18:36"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      cipassword,
+      sshkeys,
+    ]
   }
 }
